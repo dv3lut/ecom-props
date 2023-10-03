a37ad5dc3e4d5e7c80add077a0175abf2fbac63d
dv3lut <dorian.velut@diggers-consulting.com>
2023-10-03 16:59:45 +0200

    Front fixes

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -15,7 +15,7 @@ function ProductItem({product}) {
             <div className="product-info">
                 <h2 className="product-title">{product.title}</h2>
                 <p className="product-description">{product.description}</p>
-                <p className="product-category">{product.category}</p>
+                <p className="product-category">{product.category.toUpperCase()}</p>
                 <p className="product-price">{product.price} €</p>
                 <button className="product-button" onClick={handleAddProductToCart}>Ajouter au panier</button>
             </div>
